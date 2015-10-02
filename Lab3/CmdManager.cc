5d2683cc9ebaf707ffa40a527e3a02eba808abab
Zengyu Leo <zengyu.leo@gmail.com>
2015-10-03 01:33:27 +0800

    working on Cmdmaganer

--- Lab3/CmdManager.cc
+++ Lab3/CmdManager.cc
@@ -8,4 +8,31 @@
 
 
 using namespace std;
+//CmdManager类
+class CmdManager{
+public:
+	//构造函数
+	CmdManager()=default;
+	const void PrintHelp();
+	const bool workOnot(string&);
 
+private:
+string<string> CmdHelp;
+vector<string> CmdList;
+};	
+
+const bool workOnot(string $n){
+	for(it=this.CmdList.begin();it!=this.CmdList.end();++it){
+	if (n==it->name)
+		return ture;
+	}
+	return false;
+}
+
+const void PrintHelp(){
+	decltype(CmdList.size()) i;
+	for(i=0;i!=CmdList.size();++i){
+	cin<<"Comand Name:"<<CmdList[i]<<endl;
+	cin<<"Usage:"<<CmdHelp[i]<<endl;
+	} 
+}
